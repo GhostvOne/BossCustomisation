97b089da67d0014f7db1bbbafd490836a0f5386e
maliimaloo <maliimaloo27@gmail.com>
2023-01-07 22:35:49 +0100

    First Commit

--- src/main/java/fr/maliimaloo/bosscustomisation/BossCustomisation.java
+++ src/main/java/fr/maliimaloo/bosscustomisation/BossCustomisation.java
@@ -1,9 +1,12 @@
 package fr.maliimaloo.bosscustomisation;
 
+import fr.maliimaloo.omegacore.api.fichier.FichierAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.maliimaloo.bosscustomisation.event.Spawn_Boss;
 import fr.maliimaloo.omegacore.api.OmegaAPI;
+
+import java.util.ArrayList;
 
 public class BossCustomisation extends JavaPlugin {
 
@@ -20,6 +23,13 @@ public class BossCustomisation extends JavaPlugin {
 			System.out.println(" [Boss Customisation] L'instance OMEGAAPI est null");
 		else 
 			System.out.println(" [Boss Customisation] L'instance OMEGAAPI n'est pas null");
+
+		FichierAPI fichierAPI = OmegaAPI.getInstance().getFichierAPI();
+		ArrayList<String> listeFichier = new ArrayList<>();
+		listeFichier.add("Schematic");
+		listeFichier.add("Test/TEST");
+
+		fichierAPI.createFolderProject(this, listeFichier);
 	}
 	
 	@Override
